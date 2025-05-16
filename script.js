7e66e157ae76f961fbffa16e478f22d7e36d4309
Varun-Prasath <varunkprasath@gmail.com>
2025-05-16 12:31:22 +0530

    Add emoji feedback to user messages in guessing game

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+const secretNumber = Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess);
 
     if(!guess){
-        document.querySelector('.message').textContent = "No Number!!";
+        document.querySelector('.message').textContent = "⛔ No Number!!";
+    } else if(guess === secretNumber){
+        document.querySelector('.message').textContent = "🎉 Correct number";
+    } else if(guess > secretNumber){
+        document.querySelector('.message').textContent = "📈 Too High!!";
+    } else if(guess < secretNumber){
+        document.querySelector('.message').textContent = "📉 Too Low!!";
     }
 });
